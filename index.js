7b5c1e5581524c44173c37a8e1c50d32087f8717
devonhackley <devonhackley@gmail.com>
2017-08-15 14:24:30 -0700

    finished problem

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/*** Coding Challenge ***/
+
+
+module.exports = function whatTriangle(num1, num2, num3){
+
+  let n1 = num1,
+  n2 = num2,
+  n3 = num3;
+
+  if(isNaN(n1,n2,n3)) console.log(4);
+
+  if(!(isNaN(n1,n2,n3))){
+    if (n1 === n2  && n1 === n3) {
+      console.log(3);
+    } else if (n1 === n2 || n2 === n3 || n1 === n3) {
+      console.log(2);
+    } else if (n1 != n2 && n2 != n3 && n1 != n3 ) {
+      console.log(1);
+    }
+  }
+};
+
+
+
+whatTriangle(1,2,2);
