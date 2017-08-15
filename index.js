ecbe44aa322c964723ab827f3394e781378c830b
devonhackley <devonhackley@gmail.com>
2017-08-15 14:43:17 -0700

    fixed one bug when test for no arguments

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function whatTriangle(num1, num2, num3){
   n2 = num2,
   n3 = num3;
 
-  if(isNaN(n1,n2,n3)) console.log(4);
+  if(isNaN(n1,n2,n3) || null) console.log(4);
 
   if(!(isNaN(n1,n2,n3))){
     if (n1 === n2  && n1 === n3) {
@@ -24,4 +24,4 @@ function whatTriangle(num1, num2, num3){
 
 
 
-whatTriangle(1,2,2);
+whatTriangle();
